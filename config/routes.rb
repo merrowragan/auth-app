89b83df107c707aa90621142ab00d61ed3d2c422
merrow-ragan <seragan3@gmail.com>
2020-09-22 19:36:18 -0400

    practices authentications

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # EXAMPLE HTML ROUTE
+  # get "/photos" => "photos#index"
+
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  # namespace :api do
+  #   get "/photos" => "photos#index"
+  # end
+
+
+  namespace :api do
+    post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
+
+    get "/users" => "users#show" #test route
+  end
+
+end
